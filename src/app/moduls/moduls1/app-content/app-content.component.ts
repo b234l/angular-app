4ec16ca16a4660337e0a399e745a9da63cbf6d24
AlexeyLVB <korolevaleksej12@gmail.com>
2023-01-20 17:51:03 +0300

    feat: app-header, app-content and app-footer
    these components will represent the semantic structure of the WEB

---
+++ src/app/moduls/moduls1/app-content/app-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-content',
+  templateUrl: './app-content.component.html',
+  styleUrls: ['./app-content.component.scss']
+})
+export class AppContentComponent {
+
+}
