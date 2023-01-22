12b307f2d4a5ccaddbc04c0efaa08396f7720d92
AlexeyLVB <korolevaleksej12@gmail.com>
2023-01-22 20:36:07 +0300

    fix: app-footer component filled with content

--- src/app/moduls/moduls1/app-footer/app-footer.component.ts
+++ src/app/moduls/moduls1/app-footer/app-footer.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './app-footer.component.html',
   styleUrls: ['./app-footer.component.scss']
 })
-export class AppFooterComponent {
+export class AppFooterComponent implements OnInit {
+  title = 'Тверской Государственный Технический Университет';
+  year = 2023;
+
+  constructor() {}
+
+  ngOnInit(){
+  }
 
 }
