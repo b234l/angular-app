f2c16ab7c87211ce7a838b2efd676153f8a20c58
AlexeyLVB <korolevaleksej12@gmail.com>
2023-01-23 23:36:26 +0300

    fix: remove needless import

--- src/app/moduls/moduls1/moduls1.module.ts
+++ src/app/moduls/moduls1/moduls1.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from "@angular/common";
 import { AppBodyComponent } from "./app-body/app-body.component";
 import { AppHeaderComponent } from "./app-header/app-header.component";
 import { AppFooterComponent } from "./app-footer/app-footer.component";
-import { AppComponent } from "src/app/app.component";
 import { AppContentComponent } from "./app-content/app-content.component";
 
 @NgModule ({
