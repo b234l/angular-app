103ecf2b1e2a2686f99b50a810995b8bfb57655d
AlexeyLVB <korolevaleksej12@gmail.com>
2023-01-24 04:11:13 +0300

    feat: menu.component
    template of menu is taken from the PrimeNG, menu is adapted to the future content of the app
    
    BREAKING CHANGE: moduls1.module.ts and app-header.component.html
    
    MenubarModule from PrimeNG has been imported into the moduls1.module.ts, menu.component has been declared,
    to display menu, it was added as a tag in app-header.component.html

---
+++ src/app/moduls/moduls1/menu/menu.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from "primeng/menubar";
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+  items: MenuItem[] = [];
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Профиль',
+        icon: 'pi pi-fw pi-users',
+        items: [
+          { label: 'Мой профиль', icon: 'pi pi-fw pi-users', routerLink: '' },
+          { label: 'Войти в аккаунт', icon: 'pi pi-fw pi-user-plus', routerLink: '' }
+          ]
+      },
+      {
+        label: 'Обучение',
+        items: [
+          { label: 'Темы и разделы', icon: '', routerLink: '' },
+          { label: 'Задания после раздела', icon: '', routerLink: '' }
+        ]
+      },
+      {
+        label: 'Тестовые задания',
+        items: [
+          { label: 'Экзамен', icon: '', routerLink: '' },
+          { label: 'Марафон', icon: '', routerLink: '' }
+        ]
+      },
+      {
+        label: 'Полезная информация',
+        items: [
+          { label: 'Правила дорожного движения', icon: '', routerLink: '' },
+          { label: 'Информация об экзамене', icon: '', routerLink: '' },
+          { label: 'Дополнительные сведения', icon: '', routerLink: '' }
+        ]
+    }
+  ]
+  }
+}
--- src/app/moduls/moduls1/moduls1.module.ts
+++ src/app/moduls/moduls1/moduls1.module.ts
@@ -4,12 +4,19 @@ import { AppBodyComponent } from "./app-body/app-body.component";
 import { AppHeaderComponent } from "./app-header/app-header.component";
 import { AppFooterComponent } from "./app-footer/app-footer.component";
 import { AppContentComponent } from "./app-content/app-content.component";
-import { AppConfigService } from "src/app/fake-backend/services/app-config.service";
+import { AppConfigService } from "src/app/moduls/moduls1/service/app-config.service";
+import { MenuComponent } from "./menu/menu.component";
+import { MenubarModule } from "primeng/menubar";
 
 @NgModule ({
-    declarations: [AppBodyComponent, AppHeaderComponent, AppContentComponent, AppFooterComponent],
+    declarations: [AppBodyComponent, 
+        AppHeaderComponent,
+        AppContentComponent, 
+        AppFooterComponent, 
+        MenuComponent
+    ],
     exports: [AppBodyComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, MenubarModule],
     providers: [AppConfigService]
 })
 
