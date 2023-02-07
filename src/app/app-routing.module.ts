2b13b6f22851d2acb44463b6971573b84ec2a0db
AlexeyLVB <korolevaleksej12@gmail.com>
2023-02-07 03:03:10 +0300

    fix: set lazy-loading into app-routing.module.ts
    this module use path and the component assigned to it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppBodyComponent } from './moduls/moduls1/app-body/app-body.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: AppBodyComponent,
+    children: [
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
