405bd14f57e14585f3bbf5fb17b066cc3b8866a3
AlexeyLVB <korolevaleksej12@gmail.com>
2023-03-06 12:18:53 +0300

    feat: auth-guard.ts , error-interceptor.ts and jwt-interceptor
    auth-guard.ts provides protection of some restricted routes to users
    without authorization using methos CanActive() (if returns TRUE - allowed
    to proceed, if returns FALSE - route is not available)
    
    error-interceptor.ts is necessary to intercept http requests from API
    to check for errors 401 Unauthorized and 403 Forbidden. If there are any,
    user log out from app
    
    jwt-interceptor.ts intercept http requests from app to add JWT authorization
    token to Authorization header if user logged in and request is to the app
    API URL

---
+++ src/app/fake-backend/_helpers/auth-guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+import { AccountService } from '../services/account-service';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private accountService: AccountService
+  ) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const user = this.accountService.userValue;
+    if (user) {
+      return true;
+    }
+    this.router.navigate(['/account/login'], { queryParams: { returnUrl: state.url } });
+    return false;
+  }
+}
