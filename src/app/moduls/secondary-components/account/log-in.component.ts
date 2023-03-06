8b9a19288b1d89596ae43d4d757311969742bbf4
AlexeyLVB <korolevaleksej12@gmail.com>
2023-03-06 15:22:15 +0300

    feat: log-in.component
    Login component template contains log-in form with login and password
    fields. It also display validation message for invallid fields when submit
    button is clicked (form submit event - onSubmit() method)
    
    Component contains getter property 'f' to make easier to access form controls
    (f.login instead form.controls.login)
    
    status: DOESEN'T WORKING , ERRORS INTO HTML
    In log-in.component.ts (42-43) commented string with tutorial and fixed variant

---
+++ src/app/moduls/secondary-components/account/log-in.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AccountService } from 'src/app/fake-backend/services/account-service';
+import { AlertService } from 'src/app/fake-backend/services/alert-service';
+
+@Component({ templateUrl: 'log-in.component.html' })
+export class LoginComponent implements OnInit {
+  form!: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private accountService: AccountService,
+    private alertService: AlertService
+  ) { }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    this.alertService.clear();
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.accountService.login(this.f['login'].value, this.f['password'].value)
+    //this.accountService.login(this.f.login.value, this.f.password.value)
+      .pipe(first())
+      .subscribe({
+        next: () => {
+          const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+          this.router.navigateByUrl(returnUrl);
+        },
+        error: error => {
+          this.alertService.error(error);
+          this.loading = false;
+        }
+      });
+  }
+}
