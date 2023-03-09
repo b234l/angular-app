8bb12d772329fa6caf00ab431c1d35536e0074bd
AlexeyLVB <korolevaleksej12@gmail.com>
2023-03-09 04:43:05 +0300

    feat: account-service.ts
    This service provides communication between Angular and fake backend
    for all related to accounts. It contains standard CDRUD methods and
    methods for log-in, log-out and registration.
    
    status: DOESEN'T WORKING, ERRORS WITH LOCALSTORAGE (19 string) AND NULL
    (38 string)

---
+++ src/app/fake-backend/services/account-service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from 'src/environments/environment';
+import { user } from '../models/user';
+
+@Injectable({ providedIn: 'root' })
+export class AccountService {
+  private userSubject: BehaviorSubject<user>;
+  public user: Observable<user>;
+
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ) {
+    this.userSubject = new BehaviorSubject<user>(JSON.parse(localStorage.getItem('user')));
+    this.user = this.userSubject.asObservable();
+  }
+
+  public get userValue(): user {
+    return this.userSubject.value;
+  }
+
+  login(login: any, password: any) {
+    return this.http.post<user>(`${environment.apiUrl}/users/authenticate`, { login, password })
+      .pipe(map(user => {
+        localStorage.setItem('user', JSON.stringify(user));
+        this.userSubject.next(user);
+        return user;
+      }));
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.userSubject.next(null);
+    this.router.navigate(['/account/login']);
+  }
+
+  register(user: user) {
+    return this.http.post(`${environment.apiUrl}/users/register`, user);
+  }
+
+  getAll() {
+    return this.http.get<user[]>(`${environment.apiUrl}/users`);
+  }
+
+  getById(id: string) {
+    return this.http.get<user>(`${environment.apiUrl}/users/${id}`);
+  }
+
+  update(id: string, params: any) {
+    return this.http.put(`${environment.apiUrl}/users/${id}`, params)
+      .pipe(map(x => {
+        if (id == this.userValue.id) {
+          const user = { ...this.userValue, ...params };
+          localStorage.setItem('user', JSON.stringify(user));
+          this.userSubject.next(user);
+        }
+        return x;
+      }));
+  }
+
+  delete(id: string) {
+    return this.http.delete(`${environment.apiUrl}/users/${id}`)
+      .pipe(map(x => {
+        if (id == this.userValue.id) {
+          this.logout();
+        }
+        return x;
+      }));
+  }
+}
