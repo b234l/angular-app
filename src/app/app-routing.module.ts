e5d6e2199e6256586a4639e521d2f35d67a9af55
AlexeyLVB <korolevaleksej12@gmail.com>
2023-03-15 02:19:52 +0300

    fix: reconfiguration existing components
    in app.module.ts was imported learning component
    
    in app.component.html was add router-outlet. Commented part remained for
    testing fake-back, will be removed later
    
    in app-routing.module.ts was tested transition routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppBodyComponent } from './moduls/main-components/app-body/app-body.component';
+import { LearningComponent } from './moduls/secondary-components/learning/learning.component';
 
 const routes: Routes = [
-
   {
     path: '',
     component: AppBodyComponent,
     children: [
+      {path: 'learning', component: LearningComponent}
     ]
   }
-
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,16 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AccordionModule } from 'primeng/accordion';
-import { HttpClientModule } from "@angular/common/http";           
-
+import { HttpClientModule } from "@angular/common/http";    
+import { BreadcrumbModule } from 'primeng/breadcrumb';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { MainComponents } from './moduls/main-components/main-components.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
