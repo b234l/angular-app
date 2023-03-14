a05fe2f223a01bb499ea992dfd52ceddddc84ad8
AlexeyLVB <korolevaleksej12@gmail.com>
2023-03-15 02:14:56 +0300

    feat: learning.component template
    this is a approximate representation for creating structure as whole

---
+++ src/app/moduls/secondary-components/learning/learning.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-learning',
+  templateUrl: './learning.component.html',
+  styleUrls: ['./learning.component.scss']
+})
+export class LearningComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
