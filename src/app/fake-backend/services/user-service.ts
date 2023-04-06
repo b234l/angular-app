4c4c9f637ec58f91e34ff28fb657e13ec2cbb3a1
AlexeyLVB <korolevaleksej12@gmail.com>
2023-04-06 09:50:06 +0300

    feat: messange-service.ts
    this service responsible for messanges about operations from backend
    
    BREAKING CHANGES: user-service.ts
    
    full description of REST API operations

--- src/app/fake-backend/services/user-service.ts
+++ src/app/fake-backend/services/user-service.ts
@@ -1,22 +1,94 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { user } from "../models/user";
+import { Observable, of } from "rxjs";
+import { catchError, map, tap } from 'rxjs/operators';
+import { MessageService } from "./messange-service";
+import { User } from "../models/user";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class UserService {
-  baseUrl ="api/users";
-  constructor(private http:HttpClient) {
 
+  private url = 'api/users';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  
+
+  constructor(
+    private http:HttpClient,
+    private messageService: MessageService
+    ) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.url)
+    .pipe(
+      tap(_ => this.log('fetched users')),
+      catchError(this.handleError<User[]>('getUsers', []))
+    );
   }
 
-  getUsers() {
-    return this.http.get<user[]>(this.baseUrl);
+  getUser(id: number): Observable<User> {
+    const userUrl = `${this.url}/${id}`;
+    return this.http.get<User>(this.url)
+    .pipe(
+      tap(_ => this.log(`fetched user id=${id}`)),
+      catchError(this.handleError<User>(`getUser id=${id}`))
+    );
   }
 
-  addUser(user:user) {
-    return this.http.post(this.baseUrl, user);
+  addUser(user: User): Observable<User> {
+    return this.http.post<User>(this.url, user, this.httpOptions)
+    .pipe(
+      tap((newUser: User) => this.log(`added user w/ id=${newUser.id}`)),
+      catchError(this.handleError<User>('addUser'))
+    );
+  }
+
+  updateUser(user: User): Observable<any> {
+    return this.http.put(this.url, user, this.httpOptions)
+    .pipe(
+      tap(_ => this.log(`updated user id=${user.id}`)),
+      catchError(this.handleError<any>('updateUser'))
+    );
+  }
+
+  deleteUser(id: number): Observable<User> {
+    const userUrl = `${this.url}/${id}`;
+    return this.http.delete<User>(this.url, this.httpOptions)
+    .pipe(
+      tap(_ => this.log(`deleted user id=${id}`)),
+      catchError(this.handleError<User>('deleteUser'))
+    );
+  }
+
+  getUserNo404<Data>(id: number): Observable<User> {
+    const userUrl = `${this.url}/?id=${id}`;
+    return this.http.get<User[]>(userUrl)
+      .pipe(
+        map(users => users[0]),
+        tap(h => {
+          const outcome = h ? 'fetched' : 'did not find';
+          this.log(`${outcome} user id=${id}`);
+        }),
+        catchError(this.handleError<User>(`getUser id=${id}`))
+      );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.error(error);
+
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
+  private log(message: string) {
+    this.messageService.add(`UserService: ${message}`);
   }
 }
