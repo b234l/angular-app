985cbc8f78a43c98db492e97507eb486d07be6f1
AlexeyLVB <korolevaleksej12@gmail.com>
2023-05-19 16:43:20 +0300

    fix: login.component can check login to the app
    It's able to check user's data and issue an error message if they do not match the data from db.json

--- src/app/moduls/accounting-components/login/login.component.ts
+++ src/app/moduls/accounting-components/login/login.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/fake-backend/services/user-service';
+import { User } from 'src/app/fake-backend/models/user';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+  }
+  onSubmit() {
+    this.userService.getUsers().subscribe(users => {
+      const authenticatedUser = users.find(user => user.login === this.username && user.password === this.password);
+      if (authenticatedUser) {
+        // redirect to home page
+      } else {
+        this.errorMessage = 'Invalid username or password';
+      }
+    });
+  }
 
 }
